194d665c9cb1917f9e155fee3464bc66d5d1da1c
2Fe <hee861213@naver.com>
2018-10-29 22:05:58 +0900

    회원 가입 시 기입한 내용 검수 및 내부 정보 저장

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,7 +1,12 @@
 import { Component, NgZone } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform, AlertController } from 'ionic-angular';
 import { ShopPage } from '../shop/shop';
 import { PasswordPage } from '../password/password';
+import { AndroidPermissions } from '@ionic-native/android-permissions';
+import { cordovaWarn } from '@ionic-native/core';
+import { StorageProvider } from '../../providers/storage/storage';
+declare var cordova:any;
+declare var plugins:any;
 
 /**
  * Generated class for the SignupPage page.
@@ -30,7 +35,65 @@ export class SignupPage {
   passwordMask:boolean=false;
   passwordConfirmMask:boolean=false;
   
-  constructor(public navCtrl: NavController, public navParams: NavParams, private ngZone:NgZone) {
+  constructor(public platform:Platform, public navCtrl: NavController, public navParams: NavParams, private ngZone:NgZone, private androidPermissions:AndroidPermissions,
+              private alertController:AlertController, private storageProvider:StorageProvider) {
+    if(this.platform.is("android")){
+      androidPermissions.checkPermission(androidPermissions.PERMISSION.GET_ACCOUNTS).then((status)=>{
+        if(status.hasPermission){
+          console.log("Yes :D");
+          
+          plugins.DeviceAccounts.getEmail((info)=>{
+            this.ngZone.run(()=>{
+              this.email=info;
+            });
+          });
+        }else{
+          console.warn("No : ( ");
+          
+          androidPermissions.requestPermission(androidPermissions.PERMISSION.GET_ACCOUNTS).then((status)=>{
+            if(status.hasPermission){
+              console.log("call DeviceAccounts");
+              
+              plugins.DeviceAccounts.getEmail((info) => {
+                this.ngZone.run(() => {
+                  this.email = info;
+                });
+              });
+            }
+          }, (err)=>{
+
+          });
+        }
+      },(err)=>{
+
+      });
+
+      androidPermissions.checkPermission(androidPermissions.PERMISSION.SEND_SMS).then((status)=>{
+        if(status.hasPermission){
+
+        }else{
+          let alert = this.alertController.create({
+            title:'문자전송허가 없이는 앱가입이 불가능합니다.',
+            buttons:['OK'];
+          });
+          alert.present();
+          return;
+        }
+      });
+    }
+
+    cordova.plugins.email.isAvailable((available)=>{
+      console.log("hasAccount:"+available);
+      
+      if(!available){
+        let alert = this.alertController.create({
+          title:'이메일 계정 없이는 앱 가입이 불가능합니다.',
+          buttons:['OK']
+        });
+        alert.present();
+        return;
+      }
+    });
   }
 
   ionViewDidLoad() {
@@ -43,8 +106,34 @@ export class SignupPage {
       this.agreement=true;
       this.buttonColor={'color':'white', 'background-color':'#6441a5'};
     }else{
-      this.navCtrl.push(ShopPage);
+      if(!this.email|| !this.validateEmail(this.email)){
+        let alert = this.alertController.create({
+          title:'정상 이메일을 입력해주시기 바랍니다.',
+          buttons:['OK']
+        });
+        alert.present();
+        return;
+      }
+
+      if(this.password!=this.passwordConform){
+        let alert = this.alertController.create({
+          title: '결재 비밀번호가 일치하지 않습니다.',
+          buttons: ['OK']
+        });
+        alert.present();
+        return false;
+      }
+
+      // save login info, password into NativeStorage and then move into ShopPage
+      this.storageProvider.saveId(this.navParams.get("type"),this.navParams.get("id"),this.email);
+      console.log("password:"+ this.password);
+      this.storageProvider.savePassword(this.password);
+      this.navCtrl.setRoot(ShopPage);
     }
+  }
+
+  validateEmail(email){
+    return true;
   }
 
   show(title){
